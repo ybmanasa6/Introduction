a61e3e5aa84d129f0d40e6461aad7c7e11123001
Manasa <ybmanasa6@gmail.com>
2023-01-25 20:08:17 -0500

    Third commit

--- src/CoreJavaBrushUp1.java
+++ src/CoreJavaBrushUp1.java
@@ -113,26 +113,3 @@ switch(week_name)
 	}
 }
 
-/**************************************************************/
-/*
-		Scanner sc = new Scanner(System.in);
-
-		int weekno = sc.n;
-
-
-switch(weekno)
-{
-case 1: System.out.println("Sunday");break;
-case 2: System.out.println("Monday"); break;
-case 3 : System.out.println("Tuesday"); break;
-case 4 : System.out.println("Wednessday"); break;
-case 5 : System.out.println("Thursday"); break;
-case 6 : System.out.println("Friday"); break;
-case 7 : System.out.println("Saturday"); break;
-default: System.out.println("Invalid week number");
-}
-	}
-}
-
-
-*/
